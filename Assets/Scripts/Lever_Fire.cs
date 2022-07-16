393b33109beca780b0bc25b69a8e1c3c961dcd91
Esfekia <sertac@gmail.com>
2022-07-16 12:52:45 -0400

    Created custom levers for fire and other effects, however  only fire works and it only turns on now.
    Created custom levers for fire and other effects, however  only fire works and it only turns on now.

---
+++ Assets/Scripts/Lever_Fire.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever_Fire : MonoBehaviour
+{
+    public Transform startOrientation = null;
+    public Transform endOrientation = null;
+
+    MeshRenderer meshRenderer = null;
+
+    public GameObject fireEffects;
+    bool isFireOn = false;
+
+    private void Start()
+    {
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+
+    }
+
+    public void OnLeverPullStart()
+    {
+        if (isFireOn)
+        {
+            fireEffects.SetActive(false);
+        }
+        else
+        {
+            fireEffects.SetActive(true);
+        }
+
+
+    }
+
+    public void onLeverPullStop()
+    {
+        //
+    }
+
+    public void UpdateLever(float percent)
+    {
+        transform.rotation = Quaternion.Slerp(startOrientation.rotation, endOrientation.rotation, percent);
+    }
+}
